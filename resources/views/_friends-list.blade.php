ce40c25ebbfcc7b3a8d248f77fef49f0cff768c8
TunNandaAung <tunnandaaung@outlook.com>
2020-03-27 21:36:12 +0630

    Add the Following

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,17 +1,17 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 
 <ul>
 
-    @foreach(range(1, 8) as $index)
+    @foreach(auth()->user()->follows as $user)
         <li class="mb-4">
             <div class="flex items-center text-sm">
                 <img
-                        src="https://i.pravatar.cc/40"
+                        src="{{ $user->avatar }}"
                         alt=""
                         class="rounded-full mr-2"
                 >
 
-                John Doe
+                {{ $user->name }}
             </div>
         </li>
     @endforeach
