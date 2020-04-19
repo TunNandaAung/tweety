efa8f6af72483315a0e0aff9f9a276a707c7ec6c
TunNandaAung <tunnandaaung@outlook.com>
2020-04-19 19:41:46 +0630

    Fix loose ends

---
+++ resources/js/authorizations.js
@@ -0,0 +1,7 @@
+let user = window.App.user;
+
+module.exports = {
+    owns(model, prop = "user_id") {
+        return parseInt(model[prop]) === user.id;
+    }
+};
--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -2,17 +2,22 @@
     <section class="px-8">
         <main class="container mx-auto">
             <div class="lg:flex lg:justify-center">
+                @auth
                 <div class="lg:w-32">
                     @include ('_sidebar-links')
                 </div>
+                @endauth
 
                 <div class="lg:flex-1 lg:mx-10 lg:mb-10" style="max-width: 700px">
                     {{ $slot }}
                 </div>
-
-                <div class="lg:w-64">
-                    @include ('_friends-list')
-                </div>
+               
+                @auth
+                    <div class="lg:w-64">
+                        @include ('_friends-list')
+                    </div>
+                @endauth
+                
             </div>
         </main>
     </section>
