caf53d35efa161817fbcb5e8958e36ff12e09859
TunNandaAung <tunnandaaung@outlook.com>
2020-04-27 18:55:55 +0630

    Group notifications by date

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -8,9 +8,19 @@ class NotificationsController extends Controller
 {
     public function index()
     {
-        $readNotifications = current_user()->notifications->where('read_at', '!=', null);
-        $unreadNotifications = current_user()->unreadNotifications;
-        //return $notifications;
+        $readNotifications = current_user()->notifications
+                                        ->where('read_at', '!=', null)
+                                        ->groupBy(function ($item) {
+                                            return $item->created_at->format('d M y');
+                                        });
+
+        $unreadNotifications = current_user()->unreadNotifications
+                                        ->groupBy(function ($item) {
+                                            return $item->created_at->format('d M y');
+                                        });
+        //return $readNotifications;
+        current_user()->unreadNotifications->markAsRead();
+
         return view('notifications.index', compact('readNotifications', 'unreadNotifications'));
     }
 }
--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -1,51 +1,65 @@
 <x-app>
 
     @if ($unreadNotifications->count())
-        <h1 class="text-gray-800 font-bold text-xl mb-4">New Notifications</h1>
+        <h1 class="text-blue-800 font-bold text-xl mb-4">New Notifications</h1>
 
-        @foreach ($unreadNotifications as $unreadNotification) 
-        <div class="flex bg-white shadow p-4 rounded-xl mb-4 border border-gray-300">
-            
-            <div class="flex w-full">
-            
-                @include("notifications._".get_notification_view($unreadNotification->type),['notification' => $unreadNotification])
-                            
-            
-                <div class="flex-1 text-right tracking-tight">
-                    <h3 class="font-semibold broder border-r-4 {{ get_notification_color($unreadNotification->type) }} p-2">{{ $unreadNotification->created_at->format('M d') }} </h3>
-                    <span class="text-gray-700 text-sm font-bold">{{ $unreadNotification->created_at->diffForHumans() }}</span>
-                </div>
-                    
+        @foreach ($unreadNotifications as $date => $unreadNotifications)
+            <div class="mb-6 text-right">
+                <span class="px-5 py-3 rounded-xl border border-blue-500 text-blue-700 shadow-lg font-semibold text-lg">{{ $date }}</span>
             </div>
-        
-        </div>
-            
-        @endforeach
 
+            @foreach ($unreadNotifications as $unreadNotification) 
+            <div class="flex bg-white shadow p-4 rounded-xl mb-4 border border-gray-300">
+                
+                <div class="flex w-full">
+                
+                    @include("notifications._".get_notification_view($unreadNotification->type),['notification' => $unreadNotification])
+                                
+                
+                    <div class="flex-1 text-right tracking-tight">
+                        {{-- <h3 class="font-semibold broder border-r-4 {{ get_notification_color($unreadNotification->type) }} p-2">{{ $unreadNotification->created_at->format('M d') }} </h3> --}}
+                        <span class="text-gray-700 font-bold broder border-r-4 {{ get_notification_color($unreadNotification->type) }} p-2">{{ $unreadNotification->created_at->diffForHumans() }}</span>
+                    </div>
+                        
+                </div>
+            
+            </div>
+                
+            @endforeach
+
+            <hr class="mb-6 mt-6 text-gray-800 h-1">
+        @endforeach
     @endif
 
     @if ($readNotifications->count())
         <h1 class="text-gray-800 font-bold text-xl mb-4">Older</h1>
-
-        @foreach ($readNotifications as $readNotification) 
-        <div class="flex bg-white shadow p-4 rounded-xl mb-4 border border-gray-300">
-            
-            <div class="flex w-full">
-            
-                @include("notifications._".get_notification_view($readNotification->type),['notification' => $readNotification])
-                            
-            
-                <div class="flex-1 text-right tracking-tight">
-                    <h3 class="font-semibold broder border-r-4 {{ get_notification_color($readNotification->type) }} p-2">{{ $readNotification->created_at->format('M d') }} </h3>
-                    <span class="text-gray-700 text-sm font-bold">{{ $readNotification->created_at->diffForHumans() }}</span>
-                </div>
-                    
+        
+        @foreach ($readNotifications as $date => $readNotifications)
+            <div class="mb-6 text-right">
+                <span class="px-5 py-3 rounded-xl border border-blue-500 text-blue-700 shadow-lg font-semibold text-lg">{{ $date }}</span>
             </div>
         
-        </div>
+            @foreach ($readNotifications as $readNotification) 
             
-        @endforeach
+            <div class="flex bg-white shadow p-4 rounded-xl mb-4 border border-gray-300">
+                
+                <div class="flex w-full">
+                
+                    @include("notifications._".get_notification_view($readNotification->type),['notification' => $readNotification])
+                                
+                
+                    <div class="flex-1 text-right tracking-tight">
+                        {{-- <h3 class="font-semibold broder border-r-4 {{ get_notification_color($readNotification->type) }} p-2">{{ $readNotification->created_at->format('M d') }} </h3> --}}
+                        <span class="text-gray-700 font-bold broder border-r-4 {{ get_notification_color($readNotification->type) }} p-2">{{ $readNotification->created_at->diffForHumans() }}</span>
+                    </div>
+                        
+                </div>
+            
+            </div>
+            @endforeach
 
+            <hr class="mb-6 mt-6 text-gray-800 h-1">
+        @endforeach
     @endif
 
     
