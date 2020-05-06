05b71b89c73b1aae3f68a34e4dfb9e113bbdcca7
TunNandaAung <tunnandaaung@outlook.com>
2020-05-06 20:59:11 +0630

    Style explore page

--- resources/views/explore.blade.php
+++ resources/views/explore.blade.php
@@ -1,18 +1,28 @@
 <x-app>
     <div>
         @foreach($users as $user)
-            <a href={{ $user->path() }} class="flex items-center mb-5">
-                <img
-                    src="{{ $user->avatar }}"
-                    alt="{{ $user->username }}'s avatar"
-                    width="60"
-                    class="mr-4 rounded-full"
-                >
+            <div class="py-5 {{ $loop->last ? '' :  'border-b border-gray-400'}}">
+                <div class="flex">
+                    <a href={{ $user->path() }} class="flex flex-1 items-start ">
+                        <img
+                            src="{{ $user->avatar }}"
+                            alt="{{ $user->username }}'s avatar"
+                            width="60"
+                            class="mr-4 rounded-full"
+                        >
+        
+                        <div class="flex flex-col items-start">
+                            <h4 class="font-bold rounded-full px-2 py-1 -ml-2 bg-transparent hover:bg-blue-500 hover:text-white text-center block">{{ $user->name }}</h4>
+                            <span class="font-bold text-sm text-gray-600">{{ '@'. $user->username }}</span>
+                            @if(isset($user->description))
+                                <p class="mt-3">{{ $user->description }}</p>
+                            @endif
+                        </div>
+                    </a>
 
-                <div>
-                    <h4 class="font-bold rounded-full bg-transparent px-2 py-1 hover:bg-blue-500 hover:text-white text-center block">{{ '@'.$user->username }}</h4>
+                    <x-follow-button :user="$user"></x-follow-button>
                 </div>
-            </a>
+            </div>
 
         @endforeach
 
