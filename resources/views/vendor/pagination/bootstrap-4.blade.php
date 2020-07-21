3c6c329450164a8b212ce476677ea4abc876ed2d
TunNandaAung <tunnandaaung@outlook.com>
2020-07-21 21:53:28 +0630

    Update paginator stylings

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -23,7 +23,7 @@
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li class="page-item text-blue-600 bg-blue-200" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                            <li class="page-item text-blue-600 bg-blue-200 rounded-lg" aria-current="page"><span class="page-link">{{ $page }}</span></li>
                         @else
                             <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
                         @endif
